a07698b01242e8a9773be35434e131a2d81c0109
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-05-17 11:19:29 +0100

    slider custom theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -86,17 +86,27 @@ class _InputPageState extends State<InputPage> {
                       ),
                     ],
                   ),
-                  Slider(
-                    value: this.height.toDouble(),
-                    min: 120.0,
-                    max: 220.0,
-                    activeColor: Color(0xFFEB1555),
-                    inactiveColor: Color(0xFF8D8E98),
-                    onChanged: (double newValue) {
-                      setState(() {
-                        this.height = newValue.round();
-                      });
-                    },
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      activeTrackColor: Color(0xFFFFFFFF),
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      thumbColor: Color(0xFFeb1555),
+                      overlayColor: Color(0x29eb1555),
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: this.height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          this.height = newValue.round();
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
